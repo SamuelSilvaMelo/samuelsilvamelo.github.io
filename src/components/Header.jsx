2ece34e9265aeb5cc25b83ec2440c472e22eab55
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-08-07 15:41:13 -0300

    Add Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../style/components/header.css'
+import logo from '../img/logo.png'
+
+const Header = () => (
+  <header className="main-header">
+    <nav className="nav-header">
+      <a className="logo-anchor" href="home">
+          <img src={ logo } alt="Airplane icon with heart on the left side" />
+      </a>
+      <a className="about-anchor" href="#about">Sobre mim</a>
+      <a className="skills-anchor" href="#skills">Habilidades</a>
+      <a className="projects-anchor" href="#projects">Projetos</a>
+      <a className="contact-anchor" href="#contact">Contato</a>
+    </nav>
+  </header>
+);
+
+export default Header;
