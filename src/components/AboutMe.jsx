548c62f984afb33ae28461feb4f38454347a4ce4
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-09-06 10:38:18 -0300

    Add media query @990px to AboutMe

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,17 +6,19 @@ import DownArrow from './common/DownArrow';
 
 const AboutMe = () => (
   <section className="about-me" id="about">
-    <div className="hobbies-img">
-        <img src={ hobbies } alt=""/>   
-    </div>
-    <div className="about-me-div-content">
-        <div className="about-me-text">
-            <p>Olá, meu nome é Samuel Melo, tenho 22 anos, moro em Belo Horizonte e encontro-me atualmente estudando Desenvolvimento Web Full Stack na Trybe.</p>
-            <p>Nos meus momentos livres adoro fazer novos projetos, jogar videogame, conversar e estudar sobre o mundo da aviação assim como ir ao aeroporto para ver os aviões.</p>
-            <p>Minhas três paixões estão na aviação, em criar novos códigos e jogar videogame.</p> 
-        </div>
-        <img className="airplane-img" src={ airplane} alt="" />
-    </div>
+    <article className="about-me-article-content">
+      <div className="hobbies-img">
+          <img src={ hobbies } alt=""/>   
+      </div>
+      <div className="about-me-div-content">
+          <div className="about-me-text">
+              <p>Olá, meu nome é Samuel Melo, tenho 22 anos, moro em Belo Horizonte e encontro-me atualmente estudando Desenvolvimento Web Full Stack na Trybe.</p>
+              <p>Nos meus momentos livres adoro fazer novos projetos, jogar videogame, conversar e estudar sobre o mundo da aviação assim como ir ao aeroporto para ver os aviões.</p>
+              <p>Minhas três paixões estão na aviação, em criar novos códigos e jogar videogame.</p> 
+          </div>
+          <img className="airplane-img" src={ airplane} alt="" />
+      </div>
+    </article>
     <DownArrow location="#skills" />
   </section>
 );
